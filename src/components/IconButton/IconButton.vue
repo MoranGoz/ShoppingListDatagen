5958e53866ddcc440d412c76e4c8fa08dba4378c
moran <morangozlan@hotmail.com>
2022-04-30 15:25:24 +0300

    problem importing components

---
+++ src/components/IconButton/IconButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="button" onclick="buttonClicked()">
+    <img :src="imageSrc" />
+  </div>
+</template>
+
+
+<script setup lang="ts">
+  export default {
+    name: "iconButton",
+    props: ['imageSrc', 'onclick'],
+
+    methods:{
+      buttonClicked() {
+        console.log("was click")
+      }
+    }
+  }
+</script>
+
+
+
+<style src="./TheWelcome.scss" lang="scss" />
---
+++ src/views/ShoppingListPage/ShoppingListPage.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="shopping-list-page">
+    <div class="title">
+      <h1>Shopping List</h1>
+    </div>
+    <div class="list">
+        <TheWelcome />
+    </div>
+    <div class="total">
+
+    </div>
+    <div class="add-item-button">
+
+    </div>
+  </div>
+</template>
+
+<style src="./ShoppingListPage.scss" lang="scss" />
+
+
+<script>
+import Item from "@/components/Item/Item";
+import IconButton from "@/components/IconButton/IconButton";
+import TheWelcome from "@/components/TheWelcome";
+export default {
+  components: {TheWelcome, IconButton, Item}
+}
+</script>
